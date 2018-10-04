06b764e9374c018afdeeeb8cba74d37add649664
nanoninja <vincent@nanoninja.com>
2018-10-04 21:07:42 +0200

    Improve code

--- algorithme/boucle/multiplication.js
+++ algorithme/boucle/multiplication.js
@@ -1,15 +1,20 @@
-// Table de multiplication
-// console.log(process.argv[2]);
+/**
+ * Table des multiplications.
+ */
 
-var maxTable = process.argv[2] || 5;
+var n = process.argv[2] || 5;
+var max = n*n;
 
-for (var i = 1; i <= maxTable; i++) {
+for (var i = 1; i <= n; i++) {
     process.stdout.write('\n');
-    for (var j = 1; j <= maxTable; j++) {
+
+    for (var j = 1; j <= n; j++) {
         var produit = i * j;
-        if (produit.toString().length == 1) {
-            process.stdout.write(' ');
-        }
+
+        process.stdout.write(' '.repeat(
+            max.toString().length - produit.toString().length
+        ));
+
         process.stdout.write(' ' + produit);
     }
 }
